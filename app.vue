1ab9e4c5a8c357886ddf6fce90b0a4d5e0b6c0fa
John Chiu <john@makertec.net>
2022-08-31 15:43:16 +0800

    feat: install tailwind & scss

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
-    <NuxtWelcome />
+     <div class="bg-test">
+      123
+      <div class="inner">
+        i'm yellow
+      </div>
+    </div>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,18 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          // tailwindcss: {},
+          'tailwindcss/nesting': {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
+  css: [
+    '@/assets/css/index.scss',
+  ],
 })
